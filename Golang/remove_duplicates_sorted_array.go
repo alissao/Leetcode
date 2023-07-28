ee0a6fc45109dc76e2a28cfd8cc71ecb302ce4b1
alissao <alissonhbv@gmail.com>
2023-07-28 10:55:41 -0300

    Leetcode 26 remove duplicates from sorted array.

---
+++ Golang/remove_duplicates_sorted_array.go
@@ -0,0 +1,12 @@
+func removeDuplicates(nums []int) int {
+  count := 0
+
+  for i, x := range nums {
+    if i < len(nums) - 1 && x == nums[i+1] {
+      continue
+    }
+    nums[count] = nums[i]
+    count++
+  }
+  return count
+}
