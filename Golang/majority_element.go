197025e5db7f790ec13f531de273661567532130
alissao <danvil54@hotmail.com>
2023-08-09 11:39:06 -0300

    Solution for majority element without using two pointers.

--- Golang/majority_element.go
+++ Golang/majority_element.go
@@ -1,36 +1,55 @@
 package leetcode
 
 type NumCounter struct {
-  Number  int
-  Counter int
+	Number  int
+	Counter int
+}
+
+func majorityElementTwoPointers(nums []int) int {
+	var majEl NumCounter = NumCounter{-1, 0}
+	var i, numsLen int = 0, len(nums)
+	var j = numsLen - 1
+	var elegible = numsLen / 2
+
+	var tempNumCounter = NumCounter{nums[i], 0}
+
+	for i < numsLen && j >= 0 {
+
+		if tempNumCounter.Number == nums[j] {
+			tempNumCounter.Counter++
+		}
+		if i < numsLen && j == 0 {
+			if tempNumCounter.Counter > elegible && tempNumCounter.Counter > majEl.Counter {
+				majEl = tempNumCounter
+			}
+			j = numsLen - 1
+			i++
+			if i < numsLen {
+				tempNumCounter = NumCounter{nums[i], 0}
+			}
+			continue
+		}
+		j--
+	}
+
+	return majEl.Number
 }
 
 func majorityElement(nums []int) int {
-  var majEl NumCounter = NumCounter{-1, 0}
-  var i, numsLen int = 0, len(nums)
-  var j = numsLen - 1
-  var elegible = numsLen/2
+	var maj int = -1
+	var count int = 0
 
-  var tempNumCounter = NumCounter{nums[i], 0}
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			maj = nums[i]
+		}
+		if nums[i] == maj {
+			count++
+		} else {
+			count--
+		}
+	}
 
-  for ;i < numsLen && j >= 0; {
-    
-    if tempNumCounter.Number == nums[j] {
-      tempNumCounter.Counter++
-    }
-    if i < numsLen && j == 0 {
-      if tempNumCounter.Counter > elegible && tempNumCounter.Counter > majEl.Counter {
-        majEl = tempNumCounter
-      }
-      j = numsLen - 1
-      i++
-      if i < numsLen {
-        tempNumCounter = NumCounter{nums[i], 0}
-      }
-      continue
-    }
-    j--
-  }
+	return maj
 
-  return majEl.Number
 }
