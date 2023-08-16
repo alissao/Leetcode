ac7818181d4094db9577eb1ae7d2087ba23cceaa
alissao <alissonhbv@gmail.com>
2023-08-16 13:05:50 -0300

    121 best time to buy stocks. Optimization.

--- Golang/best_time_to_buy_n_sell_stock.go
+++ Golang/best_time_to_buy_n_sell_stock.go
@@ -1,23 +1,39 @@
 package leetcode
 
-func maxProfit(prices []int) int {
-  var lowest, highest, lowestIndex, highestProfit = -1, -1, 0, 0
+// func maxProfit(prices []int) int {
+//   var lowest, highest, lowestIndex, highestProfit = -1, -1, 0, 0
 
-  for x, el := range prices {
-    if (el < lowest || lowest == -1) {
-      if (highest != -1 && highest - lowest > highestProfit) {
-        highestProfit = highest - lowest 
-      }
+//   for x, el := range prices {
+//     if (el < lowest || lowest == -1) {
+//       if (highest != -1 && highest - lowest > highestProfit) {
+//         highestProfit = highest - lowest 
+//       }
+//       lowest = el
+//       lowestIndex = x
+//       highest = -1
+//     } else if (el > highest && el > lowest && x > lowestIndex) {
+//       highest = el
+//       if (highest - lowest > highestProfit) {
+//         highestProfit = highest - lowest
+//       }
+//     }
+//   }
+
+//   return highestProfit
+// }
+
+func maxProfit(prices []int) int {
+  var lowest, maxProfit = prices[0], 0
+
+  for _, el := range prices {
+    diff := el - lowest
+    if (diff > maxProfit) {
+      maxProfit = diff 
+    }
+    if (el < lowest) {
       lowest = el
-      lowestIndex = x
-      highest = -1
-    } else if (el > highest && el > lowest && x > lowestIndex) {
-      highest = el
-      if (highest - lowest > highestProfit) {
-        highestProfit = highest - lowest
-      }
     }
   }
 
-  return highestProfit
+  return maxProfit
 }
