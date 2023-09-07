a3deb6e20a0523c1133256a334b0c0093cfdbf1b
alissao <alissonhbv@gmail.com>
2023-09-07 10:14:05 -0300

    Added solution for 14. Longefst common prefix in Golang.

--- Golang/longest_common_prefix/longest_common_prefix.go
+++ Golang/longest_common_prefix/longest_common_prefix.go
@@ -1,5 +1,26 @@
 package longestcommonprefix
 
+import (
+	"sort"
+)
+
 func LongestCommonPrefix(strs []string) string {
-  
+  if (len(strs[0]) == 0) {
+		return ""
+	}
+	strsLen := len(strs)
+	
+	if (strsLen == 1) {
+		return strs[0]
+	}
+
+	sort.Strings(strs)
+
+	for i := range(strs[0]) {
+		if (strs[0][i] != strs[strsLen-1][i]) {
+			return strs[0][:i]
+		}
+	}
+
+	return strs[0]
 }
