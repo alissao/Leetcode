9e547b19fdc089a1c7b09c684b9644e9d2f5ae9f
alissao <alissonhbv@gmail.com>
2023-09-07 11:45:54 -0300

    Added solution for Leetcode 28. Find the Index of the First Occurrence in a String .

--- Golang/find_the_index_first_ocurrence_string/find_the_index_first_ocurrence_string.go
+++ Golang/find_the_index_first_ocurrence_string/find_the_index_first_ocurrence_string.go
@@ -1,6 +1,31 @@
 package firstOcurrence
 
 func StrStr(haystack string, needle string) int {
-    
-	return -1
+	if len(needle) > len(haystack) {
+		return -1
+	}
+
+	nIndex := 0
+	firstAppear := -1
+
+	for i := 0; i < len(haystack) && nIndex < len(needle); i++ {
+		if haystack[i] == needle[nIndex] {
+			if firstAppear == -1 {
+				firstAppear = i
+			}
+			nIndex++
+		} else {
+			if firstAppear != -1 {
+				i = firstAppear
+				firstAppear = -1
+			}
+			nIndex = 0
+		}
+	}
+
+	if (len(haystack) - firstAppear < len(needle)) {
+		firstAppear = -1
+	}
+
+	return firstAppear
 }
--- Golang/find_the_index_first_ocurrence_string/find_the_index_first_ocurrence_string_test.go
+++ Golang/find_the_index_first_ocurrence_string/find_the_index_first_ocurrence_string_test.go
@@ -25,7 +25,71 @@ func TestCaseFirstOcurrence_2(t *testing.T) {
 	var needle string = "leeto"
 	var haystack string = "leetcode"
 	var result = fOcurr.StrStr(haystack, needle)
-	var expected = 0
+	var expected = -1
+	if result != expected {
+		t.Fatalf(
+			`StrStr(%s, %s) should have returned %d, but the result was %d.`, 
+			haystack, 
+			needle, 
+			expected, 
+			result,
+		)
+	}
+}
+
+func TestCaseFirstOcurrence_3(t *testing.T) {
+	var needle string = "aaaa"
+	var haystack string = "aaa"
+	var result = fOcurr.StrStr(haystack, needle)
+	var expected = -1
+	if result != expected {
+		t.Fatalf(
+			`StrStr(%s, %s) should have returned %d, but the result was %d.`, 
+			haystack, 
+			needle, 
+			expected, 
+			result,
+		)
+	}
+}
+
+func TestCaseFirstOcurrence_4(t *testing.T) {
+	var needle string = "issip"
+	var haystack string = "mississippi"
+	var result = fOcurr.StrStr(haystack, needle)
+	var expected = 4
+	if result != expected {
+		t.Fatalf(
+			`StrStr(%s, %s) should have returned %d, but the result was %d.`, 
+			haystack, 
+			needle, 
+			expected, 
+			result,
+		)
+	}
+}
+
+func TestCaseFirstOcurrence_5(t *testing.T) {
+	var needle string = "issipi"
+	var haystack string = "mississippi"
+	var result = fOcurr.StrStr(haystack, needle)
+	var expected = -1
+	if result != expected {
+		t.Fatalf(
+			`StrStr(%s, %s) should have returned %d, but the result was %d.`, 
+			haystack, 
+			needle, 
+			expected, 
+			result,
+		)
+	}
+}
+
+func TestCaseFirstOcurrence_6(t *testing.T) {
+	var needle string = "pi"
+	var haystack string = "mississippi"
+	var result = fOcurr.StrStr(haystack, needle)
+	var expected = 9
 	if result != expected {
 		t.Fatalf(
 			`StrStr(%s, %s) should have returned %d, but the result was %d.`, 
