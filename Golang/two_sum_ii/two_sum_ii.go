64ae84d26116cd4c4367d8b60f175e39dd65ce2a
alissao <alissonhbv@gmail.com>
2023-10-19 12:12:37 -0300

    Leetcode 167. Two Sum II - Performance improvements.

--- Golang/two_sum_ii/two_sum_ii.go
+++ Golang/two_sum_ii/two_sum_ii.go
@@ -2,18 +2,16 @@ package twosumii
 
 func TwoSum(numbers []int, target int) []int {
 	pointer1 := 0
-	pointer2 := 1
-	numbersLen := len(numbers)
+	pointer2 := len(numbers) - 1
 
-	for ; pointer1 < numbersLen && pointer2 < numbersLen; {
+	for ; pointer1 < len(numbers) && pointer2 < len(numbers); {
 		twoSum := numbers[pointer1] + numbers[pointer2]
-		if twoSum == target {
-			break
-		}
-		pointer2++
-		if pointer2 == numbersLen {
+		if twoSum > target {
+			pointer2--
+		} else if twoSum < target {
 			pointer1++
-			pointer2 = pointer1 + 1
+		} else if twoSum == target {
+			break
 		}
 	}
 
